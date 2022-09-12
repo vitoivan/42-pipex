10922240f5780c2cd3dc94876d27b4776c101978
Victor Ivan <victor.simoes@brmalls.com.br>
2022-09-12 19:27:59 -0300

    changing error messages

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 08:17:31 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/09/11 16:10:12 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/09/12 19:26:05 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	first_command(t_pipex *pipex, char **envp)
 	close(pipex->pipe[0]);
 	filein = open(pipex->filein, O_RDONLY);
 	if (filein == -1)
-		error(ft_strjoin("Error opening file: ", pipex->filein), 1);
+		error(ft_strjoin("no such file or directory ", pipex->filein), 1);
 	dup2(filein, STDIN_FILENO);
 	dup2(pipex->pipe[1], STDOUT_FILENO);
 	cmd = get_cmd(pipex->cmd1);
@@ -29,12 +29,12 @@ void	first_command(t_pipex *pipex, char **envp)
 	if (!binary_path)
 	{
 		free_cmd_and_path(cmd, binary_path);
-		error(ft_strjoin("Error: Command not found: ", pipex->cmd1), 127);
+		error(ft_strjoin("command not found: ", pipex->cmd1), 127);
 	}
 	if (execve(binary_path, cmd, envp) == -1)
 	{
 		free_cmd_and_path(cmd, binary_path);
-		error(ft_strjoin("Error executing command\n", pipex->cmd1), 127);
+		error(ft_strjoin("cannot execute command: \n", pipex->cmd1), 127);
 	}
 }
 
@@ -47,7 +47,7 @@ void	second_command(t_pipex *pipex, char **envp)
 	close(pipex->pipe[1]);
 	fileout = open(pipex->fileout, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fileout == -1)
-		error(ft_strjoin("Error opening file: ", pipex->fileout), 1);
+		error(ft_strjoin("no such file or directory: ", pipex->fileout), 1);
 	dup2(fileout, STDOUT_FILENO);
 	dup2(pipex->pipe[0], STDIN_FILENO);
 	cmd = get_cmd(pipex->cmd2);
@@ -55,11 +55,11 @@ void	second_command(t_pipex *pipex, char **envp)
 	if (!binary_path)
 	{
 		free_cmd_and_path(cmd, binary_path);
-		error(ft_strjoin("Error: Command not found: ", pipex->cmd1), 127);
+		error(ft_strjoin("command not found: ", pipex->cmd1), 127);
 	}
 	if (execve(binary_path, cmd, envp) == -1)
 	{
 		free_cmd_and_path(cmd, binary_path);
-		error(ft_strjoin("Error executing command\n", pipex->cmd1), 127);
+		error(ft_strjoin("cannot execute command: \n", pipex->cmd1), 127);
 	}
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 17:35:51 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/09/11 16:17:42 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/09/12 19:27:43 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 static void	validate_args(int argc, char **argv)
 {
+	char	*error_message;
+
+	error_message = ft_strjoin("Invalid number of arguments\n",
+			"Usage: ./pipex filein cmd1 cmd2 fileout");
 	if (argc != 5)
-		error(ft_strjoin("Invalid number of arguments\n",
-				"Usage: ./pipex filein cmd1 cmd2 fileout"),
-			1);
+		error(error_message, 1);
 	else if (ft_strlen(argv[1]) == 0 || ft_strlen(argv[2]) == 0
 		|| ft_strlen(argv[3]) == 0 || ft_strlen(argv[4]) == 0)
-		error(ft_strjoin("Invalid number of arguments\n",
-				"Usage: ./pipex filein cmd1 cmd2 fileout"),
-			1);
+		error(error_message, 1);
+	free(error_message);
 }
 
 int	main(int argc, char **argv, char **envp)
